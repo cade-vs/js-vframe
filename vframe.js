c2aaeb9b80cf60f5b8ec9c64965eca15d5bc7a7a
Vladi Belperchinov-Shabanski <cade@bis.bg>
2018-08-01 02:53:08 +0300

    maint

--- vframe.js
+++ vframe.js
@@ -14,33 +14,61 @@ function on_document_click( event )
 { 
   var target = event.target;
   
-  if( target.tagName != 'A' )
-    return;
+  if( target.tagName == 'A'     ) 
+    return on_click_anchor( event, target );
   
+  if( target.tagName == 'INPUT' && target.type == "submit" )
+    return on_click_submit( event, target );
+
+}
+
+function on_click_anchor( event, target )
+{  
   var vframe = target.closest( ".vframe" );
   if( ! vframe )
     {
-    alert( "NOT FOUND" );
+    //alert( "NOT FOUND" );
     return;
     }
 
   var href = target.href;
-  http_get( href, function( xhr ) { http_get_handler( xhr, target, vframe ); } );
-  
-  
+  http_get( href, function( xhr ) { http_request_handler( xhr, target, vframe ); } );
+
+  event.stopPropagation(); 
+  return false; 
+}
+
+function on_click_submit( event, target )
+{
+  var vframe = target.closest( ".vframe" );
+  if( ! vframe )
+    {
+    //alert( "NOT FOUND" );
+    return;
+    }
+
+  var form = target.form;
+  var data = new FormData( form );
+  http_request( form.method, form.action, data, function( xhr ) { http_request_handler( xhr, target, vframe ); } );
+
   event.stopPropagation(); 
   return false; 
 }
 
 function http_get( url, callback )
 {
-    var xhr = new XMLHttpRequest();
-    xhr.onload = function() { callback( xhr ); };
-    xhr.open( "GET", url );
-    xhr.send( null );
+  http_request( "GET", url, null, callback );
 }
 
-function http_get_handler( xhr, target, vframe )
+function http_request( method, url, data, callback )
+{
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function() { callback( xhr ); };
+    xhr.open( method, url );
+    xhr.send( data );
+}
+
+function http_request_handler( xhr, target, vframe )
 {
   if( xhr.status == 200 )
     vframe.innerHTML = xhr.responseText;
